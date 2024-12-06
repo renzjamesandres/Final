5f4c36372d5f5b3a74a494eb2373c632ace60625
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-06 19:21:36 +0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author renzj
+ */
+public class Student {
+    private String name;
+    private int id;
+    private String grade;
+
+    public Student(String name, int id, String grade) {
+        this.name = name;
+        this.id = id;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+}
